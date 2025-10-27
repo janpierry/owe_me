1d2ba9dbe584436bede321fc598eb22808d2091e
Jan <janpierrycoelho@gmail.com>
2025-10-27 17:15:53 -0300

    chore: rename owe_me to app in app initialization

--- lib/src/app/di/dependency_injection.dart
+++ lib/src/app/di/dependency_injection.dart
@@ -1,7 +1,7 @@
-import 'package:owe_me/src/owe_me/di/features/core_injections.dart';
-import 'package:owe_me/src/owe_me/di/features/debtor_injections.dart';
-import 'package:owe_me/src/owe_me/di/features/favorite_description_injection.dart';
-import 'package:owe_me/src/owe_me/di/features/monetary_record_injections.dart';
+import 'package:owe_me/src/app/di/features/core_injections.dart';
+import 'package:owe_me/src/app/di/features/debtor_injections.dart';
+import 'package:owe_me/src/app/di/features/favorite_description_injection.dart';
+import 'package:owe_me/src/app/di/features/monetary_record_injections.dart';
 
 Future<void> init() async {
   initCoreInjections();
